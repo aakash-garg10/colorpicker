c077df9917cd7fa2ece95f1e58c1fd7ccec33ce9
Aakash Garg <aakashgarg1009@gmail.com>
2022-02-11 21:36:17 +0530

    added the slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Pallete from "./Pallete";
 //seedColors is an array
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 export class App extends Component {
   render() {
+    // console.log(generatePalette(seedColors[0]));
     return (
       <div>
-        <Pallete {...seedColors[2]} />
+        <Pallete pallete={generatePalette(seedColors[2])} />
       </div>
     );
   }
--- src/Pallete.jsx
+++ src/Pallete.jsx
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Pallete.css";
+//slider
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 class Pallete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500, format: "hex" };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  //function that sets level on the slider
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-    const colorBoxes = this.props.colors.map((color) => {
-      return <ColorBox background={color.color} name={color.name} />;
-    });
+    //colors will be an array that contains many arrays of different shades of colors
+    const colorBoxes = this.props.pallete.colors[this.state.level].map(
+      (color) => {
+        return <ColorBox background={color.hex} name={color.name} />;
+      }
+    );
 
     return (
       <div className="Pallete">
-        {/* anvbar goes here */}
-        <div className="Pallete-colors">
-          {colorBoxes}
-        </div>
+        <Slider
+          defaulValue={this.state.level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
+        <div className="Pallete-colors">{colorBoxes}</div>
         {/* footer goes here */}
       </div>
     );
