4e60a130d279dcfb8b759cac720276a5cedc7aaf
Aakash Garg <aakashgarg1009@gmail.com>
2022-02-13 14:15:50 +0530

    adding material ui and select in navbar

--- src/Pallete.jsx
+++ src/Pallete.jsx
@@ -8,24 +8,34 @@ class Pallete extends Component {
     super(props);
     this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   //function that sets level on the slider
   changeLevel(level) {
     this.setState({ level });
   }
+
+  //function that sets format on the select
+  changeFormat(val) {
+    this.setState({ format: val});
+  }
   render() {
-    const { level } = this.state;
+    const { level, format } = this.state;
     const { colors, paletteName, emoji } = this.props.pallete;
     //colors will be an array that contains many arrays of different shades of colors
     const colorBoxes = colors[level].map((color) => {
-      return <ColorBox background={color.hex} name={color.name} />;
+      return <ColorBox background={color[format]} name={color.name} />;
     });
 
     return (
       <div className="Pallete">
         {/* we are passing the lvl and changeLevel as we need to update them  */}
-        <NavBar level={level} changeLevel={this.changeLevel} />
+        <NavBar
+          level={level}
+          changeLevel={this.changeLevel}
+          changeFormat={this.changeFormat}
+        />
         <div className="Pallete-colors">{colorBoxes}</div>
         {/* footer goes here */}
       </div>
