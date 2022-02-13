3860b38aec7466421858fe46ff588d128e301e53
Aakash Garg <aakashgarg1009@gmail.com>
2022-02-13 15:34:03 +0530

    fixed pallete routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import Pallete from "./Pallete";
 //seedColors is an array
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
-
 import { Routes, Route, Link } from "react-router-dom";
 
 export class App extends Component {
+  //findPalette is a function that takes in a palette id and returns the palette with that id
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     // console.log(generatePalette(seedColors[0]));
     return (
       <div>
         <Routes>
-          <Route path="/" element={<Pallete />} />
-          <Route path="/palette/:paletteId" element={<Pallete />} />
-          <Pallete pallete={generatePalette(seedColors[2])} />
+          <Route
+            exact
+            path="/"
+            render={() => <h1>PALETTE LIST GOES HERE</h1>}
+          />
+          <Route
+            exact
+            path="/palette/:id"
+            render={(routeProps) => (
+              <Pallete
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
         </Routes>
+        {/* this was previously used to render before the use of react router dom */}
+        {/* <Pallete pallete={generatePalette(seedColors[2])} /> */}
       </div>
     );
   }
