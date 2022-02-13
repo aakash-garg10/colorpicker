703c3179623d98ed2d21e8fb4789e08c86871a14
Aakash Garg <aakashgarg1009@gmail.com>
2022-02-13 15:26:31 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Pallete from "./Pallete";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
+import { Routes, Route, Link } from "react-router-dom";
+
 export class App extends Component {
   render() {
     // console.log(generatePalette(seedColors[0]));
     return (
       <div>
-        <Pallete pallete={generatePalette(seedColors[2])} />
+        <Routes>
+          <Route path="/" element={<Pallete />} />
+          <Route path="/palette/:paletteId" element={<Pallete />} />
+          <Pallete pallete={generatePalette(seedColors[2])} />
+        </Routes>
       </div>
     );
   }
